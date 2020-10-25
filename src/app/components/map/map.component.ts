9bf91cadeadb3f857d4ff31ba4c773d1f49c125d
jerome <jerome.henry3@gmail.com>
2020-10-25 18:21:37 +0100

    fix

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, DoCheck, NgZone } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Control, latLng, Map, Marker, tileLayer } from 'leaflet';
 import * as L from 'leaflet';
@@ -9,20 +9,20 @@ import * as L from 'leaflet';
   styleUrls: ['./map.component.scss'],
   // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, DoCheck {
   // public locControlForm = new FormControl();
   // public locControl: boolean;
-  userlocation: any;
-  marker: L.CircleMarker[];
+  userlocation: L.LocationEvent;
+  marker: L.CircleMarker[] = [];
 
-  constructor() { }
-  options = {
-      layers: [
-          tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-      ],
-      zoom: 17,
-      center: latLng(48.41645121307068,  2.7258886999698015),
-      tms: true
+  constructor(private zone: NgZone) { }
+  options: L.MapOptions = {
+    layers: [
+        tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 17,
+    center: latLng(48.41645121307068,  2.7258886999698015),
+    zoomControl: true,
   };
   layersProviders = [
     'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', // TOPO Max zoom 17
@@ -46,18 +46,52 @@ export class MapComponent implements OnInit {
     // map.locate({setView: true, maxZoom: 16});
   }
 
+  ngDoCheck(): void {
+  }
+
   onMapReady(map: Map) {
     this.map = map;
     // map.locate({setView : false}).on('locationfound', console.log);
-    map.locate({watch: true}).on('locationfound', (event) => {
+    this.map.locate({watch: true, enableHighAccuracy: true}).on('locationfound', (event) => {
       console.log('event', event);
-      this.userlocation = event.latlng;
+      console.log('map', map);
+      this.userlocation = event;
+      const distance = this.userlocation.latlng.distanceTo(event.bounds.getNorthEast());
+      console.log('distance', distance);
       this.marker = [
-        new L.CircleMarker(this.userlocation, {radius: 1, color: 'red'}),
-        new L.CircleMarker(this.userlocation, {radius: event.accuracy / 2})
+        new L.CircleMarker(this.userlocation.latlng, {radius: 1, color: 'red'}),
+        new L.Circle(this.userlocation.latlng, {radius: event.accuracy / 2}),
+        // new L.CircleMarker(this.userlocation.latlng, {radius: distance}),
+        // new L.CircleMarker(event.bounds.getNorthEast(), {radius: 1, color: 'red'}),
+        // new L.CircleMarker(event.bounds.getSouthWest(), {radius: 1, color: 'red'}),
       ];
-    });
+      // if (this.map.hasLayer(this.marker[1])) {
+      //   this.marker[1].remove();
+      // }
+      // this.marker.map(m => {
+      //   m.addTo(this.map);
+      // });
+    }).on('locationerror', console.error);
     // map.addControl(new Control());
+  }
+
+  onZoomEnd($event) {
+    console.log('onzoomend', $event);
+    // if (this.map.hasLayer(this.marker[1])) {
+    //   console.log('marker will be removed')
+    //   this.map.removeLayer(this.marker[1])
+    // }
+    // this.marker = [
+    //   new L.CircleMarker(this.userlocation.latlng, {radius: this.userlocation.accuracy / 2}),
+    // ]
+    // this.map.addLayer(this.marker[0]);
+    // this.marker = [
+    //   new L.CircleMarker(this.userlocation.latlng, {radius: 1, color: 'red'}),
+    //   new L.CircleMarker(this.userlocation.latlng, {radius: this.userlocation.accuracy / 2}),
+    //   // new L.CircleMarker(this.userlocation.latlng, {radius: distance}),
+    //   new L.CircleMarker(this.userlocation.bounds.getNorthEast(), {radius: 1, color: 'red'}),
+    //   new L.CircleMarker(this.userlocation.bounds.getSouthWest(), {radius: 1, color: 'red'}),
+    // ];
   }
 
   onClick($event) {
